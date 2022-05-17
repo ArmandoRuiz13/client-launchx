27f2e5c9714ec005570ea09017f20fb4182a34cb
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-05-17 10:05:15 -0500

    Resolviendo problema

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-light bg-white">
+      <router-link to="/" class="navbar-brand">LaunchX</router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/explorers" class="nav-link">Explorers</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/add" class="nav-link">Agregar Explorer</router-link>
+        </li>
+        <li class="nav-item">
+          <b class="nav-link">||</b>
+        </li>
+        <li class="nav-item">
+          <router-link to="/missioncommander" class="nav-link">MissionCommander</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/addm" class="nav-link">Agregar MissionCommander</router-link>
+        </li>
+      </div>
+    </nav>
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "app"
+};
+</script>
